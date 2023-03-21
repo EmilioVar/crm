943e515ebd6eea3c106dcdea8eb32412e6f54004
Emilio Vargas Millan <emiliovm@outlook.es>
2023-03-22 00:19:57 +0100

    livewire

---
+++ resources/views/livewire/product-create.blade.php
@@ -0,0 +1,13 @@
+<form wire:submit.prevent="submit">
+    <div class="form-group">
+      <label for="name">Nombre</label>
+      <input id="name" type="text" class="form-control" wire:model="name" placeholder="Nombre">
+      @error('name') <span class="error">{{ $message }}</span> @enderror
+    </div>
+    <div class="form-group">
+      <label for="price">Precio</label>
+      <input wire:model="price" type="number" class="form-control" id="price" placeholder="Precio">
+      @error('price') <span class="error">{{ $message }}</span> @enderror
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
