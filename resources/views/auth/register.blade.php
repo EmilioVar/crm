9a814ba5e4e432f56988e1ee0f4aea63518ffa4e
Emilio Vargas Millan <emiliovm@outlook.es>
2023-04-02 23:21:59 +0200

    fix: in register, syntax error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
                             <div class="col-md-6">
                                 <div class="form-floating">
                                     <select name="rol" class="form-select" id="rol" aria-label="Floating label select example">
-                                        <option value="">Ná de Ná/option</option>
+                                        <option value="">Ná de Ná</option>
                                       <option value="admin">Administrador</option>
                                       <option value="vendor">Vendedor</option>
                                     </select>
